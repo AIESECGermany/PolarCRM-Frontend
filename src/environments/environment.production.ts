08463e6d24623f5a43ff165cfdf279e628774172
Richard Paucar <66119217+richie-pa@users.noreply.github.com>
2024-02-28 14:49:10 +0100

    Add files via upload

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    googleClientId: '623412280142-j0hsfsfnkbid72v8428qvfmj7sptvr6u.apps.googleusercontent.com',
+    serverUrl: 'https://ec2-52-91-175-176.compute-1.amazonaws.com/'
+};
