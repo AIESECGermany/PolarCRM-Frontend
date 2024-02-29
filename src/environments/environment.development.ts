307c1d954f8e647fe6b7f3dd1e3e4d031195609b
Richard Paucar <66119217+richie-pa@users.noreply.github.com>
2024-02-29 17:41:32 +0100

    Create environment.development.ts

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    googleClientId: '623412280142-j0hsfsfnkbid72v8428qvfmj7sptvr6u.apps.googleusercontent.com',
+    serverUrl: 'https://polarcrm-backend.onrender.com/'
+};
